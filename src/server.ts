f53d6548492f32f518cfab0b1ea40104ef28ec88
Antonia <antoniatch@zohomail.eu>
2023-12-13 22:47:29 +0100

    initial commit

---
+++ src/server.ts
@@ -0,0 +1,31 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Book {
+    title: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const resolvers = {
+   Query: {
+      books: () => {
+         console.log("Hello");
+      },
+   },
+};
+
+const server = new ApolloServer({
+   typeDefs,
+   resolvers,
+});
+
+const { url } = await startStandaloneServer(server, {
+   listen: { port: 4000 },
+});
+
+console.log(`🚀  Server ready at: ${url}`);
