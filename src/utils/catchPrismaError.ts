f3f5ca122a1a422796c1fa879bb62c1c7aefe6ae
Antonia <antoniatch@zohomail.eu>
2024-01-21 16:10:12 +0100

    Ajout de variables dans catchPrismaError

--- src/utils/catchPrismaError.ts
+++ src/utils/catchPrismaError.ts
@@ -1,12 +1,20 @@
 import { GraphQLError } from "graphql";
 
 export const catchPrismaError = (error): void => {
+    console.log(error);
     const errorMessage: string | undefined = error.message ?? undefined;
 
     const clientVersion = error.clientVersion ?? error.extensions?.clientVersion ?? undefined;
     const code = error.code ?? error.errorCode ?? error.extensions?.code ?? "UNKNOWN";
-    const target = error.meta?.target[0] ?? error.extensions?.target ?? undefined;
+    const target = error.meta?.target
+        ? error.meta.target[0]
+        : error.extensions?.target
+          ? error.extensions.target
+          : undefined;
+    const modelName = error.meta?.modelName ?? error.extensions.modelName ?? undefined;
+    const cause = error.meta?.cause ?? error.extensions.cause ?? undefined;
 
+    // Si clientVersion existe, cela signifie que c'est une erreur générée par Prisma
     if (clientVersion) {
         if (code === "P2002" && target === "email")
             throw new GraphQLError("Un utilisateur existe déjà avec cet e-mail", {
@@ -14,6 +22,8 @@ export const catchPrismaError = (error): void => {
                     code,
                     target,
                     clientVersion,
+                    modelName,
+                    cause,
                 },
             });
 
@@ -23,6 +33,8 @@ export const catchPrismaError = (error): void => {
                     code,
                     target,
                     clientVersion,
+                    modelName,
+                    cause,
                 },
             });
 
@@ -31,6 +43,8 @@ export const catchPrismaError = (error): void => {
                 code,
                 target,
                 clientVersion,
+                modelName,
+                cause,
             },
         });
     }
